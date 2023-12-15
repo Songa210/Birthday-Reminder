26f520f8d813574aba0ed77de806b7fe5e501946
Elyse NIYOMWUNGERE <elysesonga1997@gmail.com>
2023-12-15 16:39:57 +0200

    first commit

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: "Elyse Songa Pro",
+    age: 29,
+    image:
+      "https://z-p3-scontent-mba1-1.xx.fbcdn.net/v/t39.30808-6/339079451_473265358266095_3776957412526622627_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=9c7eae&_nc_eui2=AeGLZrXAaKlzGMpvhAeJN8cDRZGCffoKGOxFkYJ9-goY7Nf24wK9ap2VsCjU54edz2bnqPWEv0wX9-YbGNXBvZ0t&_nc_ohc=nrAW9EOGuwkAX-B6M-X&_nc_zt=23&_nc_ht=z-p3-scontent-mba1-1.xx&oh=00_AfAaVrytcyIboSsnlwVkIBihfI7O65lRONs_eu6Ea-RMug&oe=6581748E",
+  },
+  {
+    id: 2,
+    name: "Elissa MUNYENTWARI",
+    age: 32,
+    image:
+      "https://z-p3-scontent-mba1-1.xx.fbcdn.net/v/t1.6435-9/84349796_534029334138661_1010978968687345664_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=7a1959&_nc_eui2=AeHkUiXP0vAZ7gnpCrgnkGUEWMIZcZX6cJpYwhlxlfpwmk3aQjmEl8g-W4HlJmqfBvZaAO2GBE0oXeWUkESDtPFr&_nc_ohc=T9dtyL-rr8gAX86EFmk&_nc_ht=z-p3-scontent-mba1-1.xx&oh=00_AfCQ-Z1dRnjHtgzoAlUlFzfGUQRx7ds1xgAoj1l-mb60kQ&oe=65A3E2CC",
+  },
+  {
+    id: 3,
+    name: "Chadrack NTIBANYURWA",
+    age: 36,
+    image:
+      "https://z-p3-scontent-mba1-1.xx.fbcdn.net/v/t39.30808-6/294625170_177482764686648_3712313264034707163_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=9c7eae&_nc_eui2=AeF2mrPNtrNN5GgSjfkJutvbE15vlaYZjYwTXm-VphmNjPU7we1MM6Xg4tY1-RsadU6en9zajDHfONodlk1dg9ta&_nc_ohc=m6AefK25Pw4AX-i515R&_nc_zt=23&_nc_ht=z-p3-scontent-mba1-1.xx&oh=00_AfCXPolmrtaY-zJauJN6A-hLP37h5Kz2KEicbljmWYnCvg&oe=65814BA7",
+  },
+  {
+    id: 4,
+    name: "Elvis NDATIMANA",
+    age: 34,
+    image:
+      "https://z-p3-scontent-mba1-1.xx.fbcdn.net/v/t39.30808-6/398320044_1060187385164835_3848188861112282920_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=efb6e6&_nc_eui2=AeHp8tO8ukH3bTf_3CvatvQg29fNlmmRUe_b182WaZFR7-ycaMsWh9Sn8h0MGk7QaSAxr7d62sSvH4QHIqKqZz68&_nc_ohc=mpo-heW13IAAX-OSb93&_nc_oc=AQkJ5c-YeDecCixw61Y9jbJTIC7Kiw23LVM73dZ8TZkEouNB6VpzagdlKiYYyJ41PO4&_nc_zt=23&_nc_ht=z-p3-scontent-mba1-1.xx&oh=00_AfDAIwZ-BXuMSkhmbzSk_3EBTmJoislDDFScN2QPmlye5g&oe=65809F26",
+  },
+  {
+    id: 5,
+    name: "Appolinaire Gasangwa",
+    age: 29,
+    image:
+      "https://z-p3-scontent-mba1-1.xx.fbcdn.net/v/t39.30808-6/318122832_1251921882030381_497670857020828539_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=9c7eae&_nc_eui2=AeHZQzLuO6HwCv0RZfz4hnM6RCIKZuMYNg9EIgpm4xg2D65DaxjgxfrpfkKd-h7dI2Jqk9LMkF587HGg0hmrSalM&_nc_ohc=hYuuaVG9AGMAX_YU7F2&_nc_zt=23&_nc_ht=z-p3-scontent-mba1-1.xx&oh=00_AfCe3FUNIIj4OgravyjhHGg2CHY7WbmgI0K3W6MSDMFOKg&oe=65815C30",
+  },
+];
