2f54076f0dbaca3f553e9d4fc04d081ad654fec8
Elyse NIYOMWUNGERE <elysesonga1997@gmail.com>
2023-12-16 14:43:03 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
 function App() {
-  const [peaple,setPeaple] = useState(data)
+  const [peaple, setPeaple] = useState(data);
   return (
     <main>
-      <section className='container'>
-        <h3>{ peaple.length} birthdays today</h3>
-        <List peaple={ peaple} />
-        <button onClick={() => console.log('you clicked me')}>Clear all
-
-        </button>
+      <section className="container">
+        <h3>{peaple.length} birthdays today</h3>
+        <List peaple={peaple} />
+        <button onClick={() => console.log("you clicked me")}>Clear all</button>
       </section>
     </main>
-  )
+  );
 }
 
 export default App;
